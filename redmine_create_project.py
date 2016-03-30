f87225db62f2557fed2469e971bb7e06d253bef6
José Domingo <josedom24@gmail.com>
2016-03-30 13:17:17 +0200

    Create project ok

--- redmine_create_project.py
+++ redmine_create_project.py
@@ -6,17 +6,19 @@ payload = {'key':'466f4055fe2f206676793d544b06ddee64b45432'}
 xml_project="""
 <project>
 	<name>test project</name>
-	<identifier>test</identifier>
+	<identifier>test1</identifier>
 	<enabled_module_names>time_tracking</enabled_module_names>
 	<enabled_module_names>issue_tracking</enabled_module_names>
 </project>
 """
 
 
+headers = {'Content-Type': 'application/xml'}
+r = requests.post('http://dit.gonzalonazareno.org/redmine/projects.xml', params=payload, data=xml_project, headers=headers)
 
-r=requests.post('http://dit.gonzalonazareno.org/redmine/projects.xml',data={'project':xml_project,'key':payload["key"]})
-print r.body	
-print r.status_code
-if r.status_code == 201:
+
+if r.status_code==201:
 	print "ok"
+else:
+	print "Error: "+r.text
 	
