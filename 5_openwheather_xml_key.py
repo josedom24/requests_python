40401e9729decac0a0ce1f352800b632af8848e8
José Domingo Muñoz <josedom24@gmail.com>
2016-04-04 09:35:23 +0200

    Cambio nombre

--- 5_openwheather_xml_key.py
+++ 5_openwheather_xml_key.py
@@ -6,7 +6,6 @@ ciudad = raw_input("Ciudad:")
 payload = {'q': ciudad, 'mode': 'xml','units':'metric','APPID':'224045b10874d0e48810401aa1455c4e'}
 r=requests.get('http://api.openweathermap.org/data/2.5/weather',params=payload)
 if r.status_code == 200:
-	print r.encoding
 	doc = etree.fromstring(r.text.encode ('utf-8'))
 	temp=doc.find("temperature").attrib["value"]
 	print temp+" ºC"
