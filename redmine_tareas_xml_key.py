5bd68126a1d83af1fb75f7763c49c0de341965d8
José Domingo Muñoz <josedom24@gmail.com>
2016-04-04 09:18:30 +0200

    Cambio de nombre

--- redmine_tareas_xml_key.py
+++ redmine_tareas_xml_key.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import requests
 from lxml import etree
-import os
 
-payload = {'limit':'100','id':'24','key':os.environ['key']}
+
+payload = {'limit':'100','id':'24','key':'466f4055fe2f206676793d544b06ddee64b45432'}
 r=requests.get('http://dit.gonzalonazareno.org/redmine/issues.xml',params=payload)
 if r.status_code == 200:
 	doc = etree.fromstring(r.text.encode ('utf-8'))
