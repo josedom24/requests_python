d56142ce102e243ace6f66e21eee8d4123c992fc
José Domingo <jose@profe1asir.gonzalonazareno.org>
2017-03-20 14:31:32 +0100

    redmine isseus

---
+++ 9_redmine_issues_xml.py
@@ -0,0 +1,28 @@
+import requests
+from lxml import etree
+import os
+URL_BASE="http://dit.gonzalonazareno.org/redmine"
+payload={"key":os.environ["key_redmine"]}
+proyecto=raw_input("Proyecto:")
+r=requests.get(URL_BASE+'/projects.xml',params=payload)
+if r.status_code == 200:
+	doc = etree.fromstring(r.text.encode ('utf-8'))
+	projects=doc.findall("project/name")
+	ids=doc.findall("project/id")
+	encontrado=False
+	for p,id in zip(projects,ids):
+		if p.text==proyecto:
+			encontrado=True
+			payload["limit"]=5
+			payload["project_id"]=id.text
+			r=requests.get(URL_BASE+'/issues.xml',params=payload)
+			if r.status_code==200:
+				doc = etree.fromstring(r.text.encode ('utf-8'))
+				names=doc.findall("issue/subject")
+				descriptions=doc.findall("issue/description")	
+				for name,desc in zip(names,descriptions):
+					print name.text
+					print desc.text
+
+	if not encontrado:
+		print "Proyecto no existe"
