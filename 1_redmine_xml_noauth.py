70959e4eb5eb1352b12153fa25d08a95531c76ac
José Domingo Muñoz <josedom24@gmail.com>
2020-04-28 09:06:10 +0200

    ejercicio1

--- 1_redmine_xml_noauth.py
+++ 1_redmine_xml_noauth.py
@@ -1,9 +1,9 @@
 import requests
 from lxml import etree
-
-r=requests.get('http://dit.gonzalonazareno.org/redmine/projects.xml')
+URL_BASE="https://dit.gonzalonazareno.org/redmine/"
+r=requests.get(URL_BASE+'projects.xml')
 if r.status_code == 200:
 	doc = etree.fromstring(r.text.encode('utf-8'))
-	projects=doc.xpath("project/name")
+	projects=doc.xpath("project/name/text()")
 	for p in projects:
-		print (p.text)
+		print (p)
